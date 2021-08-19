5b80fcc2c5d3b8f3e5384cd8872d81d62b7e87f6
nym0101 <roosternam@gmail.com>
2021-08-19 09:54:32 +0900

    [Prac] study for Springboot JPA

--- bookmanager/src/main/java/com/fastcampus/jpa/bookmanager/domain/Book.java
+++ bookmanager/src/main/java/com/fastcampus/jpa/bookmanager/domain/Book.java
@@ -1,4 +1,35 @@
 package com.fastcampus.jpa.bookmanager.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor // Entity객체는 기본생성자 필수
+@Data
 public class Book {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String author;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
